ca728079a95de58c1b441bfbbe94920647dfc0c1
B4JD1K <bartlomiej.noworyta.96@gmail.com>
2023-09-12 17:08:35 +0200

    changed style of transition (to left or right) to dynamic - based on active tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
-import {animate, query, style, transition, trigger} from "@angular/animations";
+import {animate, group, query, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -8,52 +8,95 @@ import {animate, query, style, transition, trigger} from "@angular/animations";
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('routeAnim', [
-      transition('* => *', [
+      transition(':increment', [
         style({
           position: 'relative',
+          overflow: 'hidden',
         }),
 
         query(':enter, :leave', [
           style({
+            // display: 'block',
             position: 'absolute',
             top: 0,
             left: 0,
             width: '100%',
-          })
-        ], {optional: true}),
-
-        query(':enter', [
-          style({
-            opacity: 0,
-          })
-        ], {optional: true}),
-
-        query(':leave', [
-          style({
-            display: 'block',
-          }),
-          animate(500, style({
-            opacity: 0,
-          }))
-        ], {optional: true}),
-
-        query(':enter', [
-          style({
-            opacity: 0,
-            display: 'block',
             height: '100%',
-          }),
-          animate(500, style({
-            opacity: 1,
-          }))
+          })
         ], {optional: true}),
+
+        // query(':enter', [
+        // style({opacity: 0,})
+        // ], {optional: true}),
+
+        group([
+          query(':leave', [
+            animate('300ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(-80px)',
+            }))
+          ], {optional: true}),
+
+          query(':enter', [
+            style({
+              opacity: 0,
+              transform: 'translateX(80px)',
+            }),
+            animate(300, style({
+              opacity: 1,
+              transform: 'translateX(0px)',
+            }))
+          ], {optional: true}),
+        ])
+      ]),
+      transition(':decrement', [
+        style({
+          position: 'relative',
+          overflow: 'hidden',
+        }),
+
+        query(':enter, :leave', [
+          style({
+            // display: 'block',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          })
+        ], {optional: true}),
+
+        // query(':enter', [
+        // style({opacity: 0,})
+        // ], {optional: true}),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-in', style({
+              opacity: 0,
+              transform: 'translateX(80px)',
+            }))
+          ], {optional: true}),
+
+          query(':enter', [
+            style({
+              opacity: 0,
+              transform: 'translateX(-80px)',
+            }),
+            animate(300, style({
+              opacity: 1,
+              transform: 'translateX(0px)',
+            }))
+          ], {optional: true}),
+        ])
       ])
+
     ])
   ]
 })
 export class AppComponent {
   prepareRoute(outlet: RouterOutlet) {
-    if (outlet.isActivated) return outlet.activatedRoute.snapshot.url
+    if (outlet.isActivated) return outlet.activatedRouteData['tab']
     return null
   }
 }
